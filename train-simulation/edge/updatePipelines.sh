f28ff8946f906e7342804487fd1a58877350886b
popples <klauspopp@gmx.de>
2021-06-21 08:31:24 +0200

    create global pipeline instead ofsingle pipeline for each app (#13)

--- train-simulation/edge/updatePipelines.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PROJECT=train-simulator-edge-demo
-fly -t prod set-pipeline -p ${PROJECT} -c pipeline.yaml -l ci/config.yaml  -l ci/credentials.yaml
-fly -t prod set-pipeline -p ${PROJECT}-pull-requests -c pipeline-pullrequests.yaml -l ci/config.yaml  -l ci/credentials-pullrequests.yaml
---
+++ updatePipelines.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# For each concourse pipeline, create an entry in IMAGES and a corresponding entry in CONTEXTS
+#  entry in IMAGES = the name of the docker image to build, without specifying the repository and tag.
+#                    This also defines the name of the pipeline in concourse (demo-<image-name>)
+#  entry in CONTEXTS = The docker build context (path to the directory within this repo)
+#
+
+IMAGES=(
+  gps-to-ads-app
+  train-simulator-edge-app
+  )
+
+CONTEXTS=(
+  GPS-demo/gps-to-ads-app/src
+  train-simulation/edge/src
+)
+
+for i in "${!IMAGES[@]}"; do
+  IMAGE_BASENAME=${IMAGES[i]}
+  CONTEXT=${CONTEXTS[i]}
+  echo "***********"
+  echo "setting pipelines for ${IMAGE_BASENAME}, context=${CONTEXT}"
+  echo "***********"
+
+  fly -t prod set-pipeline -p demo-${IMAGE_BASENAME} -c pipeline.yaml -l ci/config.yaml  -l ci/credentials.yaml  \
+    -v docker-image-basename=${IMAGE_BASENAME} -v docker-context=${CONTEXT}
+
+  fly -t prod set-pipeline -p demo-${IMAGE_BASENAME}-pull-requests -c pipeline-pullrequests.yaml -l ci/config.yaml  -l ci/credentials-pullrequests.yaml \
+    -v docker-image-basename=${IMAGE_BASENAME} -v docker-context=${CONTEXT}
+
+done
