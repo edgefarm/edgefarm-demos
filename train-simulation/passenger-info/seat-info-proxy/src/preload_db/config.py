55977049a3606d515954efe08958e181cd314bfd
Florian Reinhold <florian.reinhold@ci4rail.com>
2021-06-25 12:50:00 +0200

    removed (and changed) postgress connection string with pw

--- train-simulation/passenger-info/seat-info-proxy/src/preload_db/config.py
+++ train-simulation/passenger-info/seat-info-proxy/src/preload_db/config.py
@@ -1,3 +1,1 @@
-DATABASE_URI = 'postgresql+psycopg2://dbst@edgefarm-postgres-prod:jw8s0F4adsslll388fggei@edgefarm-postgres-prod.postgres.database.azure.com:5432/dbst'
-
 DATABASE_URI = 'sqlite:///sales.db'
