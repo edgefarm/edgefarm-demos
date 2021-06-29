272ec0dd9dda6210ab2fdd26b7adff97f9cdd5ae
KairaMiu <>
2021-06-29 11:05:10 +0200

    Review + update to latest node-red trainsim.

--- train-simulation/simulator/config/node-red/entrypoint.sh
+++ train-simulation/simulator/config/node-red/entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Commit of node-red.trainsim repository
-TRAINSIM_COMMIT="b4a68f144e33fba3b053ff92503e352426f2fdc8"
-REPOSITORY="node-red.trainsim"
-
 DIR=`pwd`
 
 cd /data
 
 wget "https://github.com/edgefarm/${REPOSITORY}/archive/${TRAINSIM_COMMIT}.zip"
-unzip ${TRAINSIM_COMMIT}.zip
-cp ${REPOSITORY}-${TRAINSIM_COMMIT}/* .
+unzip -jo ${TRAINSIM_COMMIT}.zip "${REPOSITORY}-${TRAINSIM_COMMIT}/*" -d .
 rm ${TRAINSIM_COMMIT}.zip
-rm -r ${REPOSITORY}-${TRAINSIM_COMMIT}
 mv flows_cred.json flow_cred.json
 
 npm install
